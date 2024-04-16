98c4949513db2435904ba462a93aebd22a04b011
WeylinV <st10390916@vcconnect.edu.za>
2024-04-16 12:11:55 +0200

    Added Login and Sign Up controllers

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ST10390916_CLDV_POE.Models;
+
+namespace ST10390916_CLDV_POE.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private readonly LoginModel login;
+
+        public LoginController()
+        {
+            login = new LoginModel();
+        }
+
+        [HttpPost]
+        public ActionResult Privacy(string email, string name)
+        {
+            var loginModel = new LoginModel();
+            int userID = loginModel.SelectUser(email, name);
+            if (userID != -1)
+            {
+                // User found, proceed with login logic (e.g., set authentication cookie)
+                // For demonstration, redirecting to a dummy page
+                return RedirectToAction("Index", "Home", new { userID = userID });
+            }
+            else
+            {
+                // User not found, handle accordingly (e.g., show error message)
+                return View("LoginFailed");
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
