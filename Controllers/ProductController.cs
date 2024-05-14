2f20d3996c668ba89589c88d8c9fb98b298421a7
WeylinV <st10390916@vcconnect.edu.za>
2024-05-14 09:50:59 +0200

    Added product controller.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ST10390916_CLDV_POE.Models;
+using System.IO.Compression;
+
+namespace ST10390916_CLDV_POE.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        public Product productTbl = new Product();
+
+        [HttpPost]
+        public ActionResult AddProduct(Product product)
+        {
+            var result = productTbl.InsertProduct(product);
+            List<Product> products = Product.GetAllProducts();
+            ViewData["products"] = products;
+            return RedirectToAction("Shop", "Product");
+        }
+
+        [HttpGet]
+        public ActionResult Shop()
+        {
+            List<Product> products = Product.GetAllProducts();
+            ViewData["products"] = products;
+            return View(products);
+        }
+
+    }
+}
