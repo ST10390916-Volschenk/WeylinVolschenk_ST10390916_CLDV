48a33a3b8f57c0de276a5d69fc8c9116bc2342fb
WeylinV <st10390916@vcconnect.edu.za>
2024-05-14 09:52:44 +0200

    Added new user model.

---
+++ Models/User.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using ST10390916_CLDV_POE.Controllers;
+using System.Data.SqlClient;
+
+namespace ST10390916_CLDV_POE.Models
+{
+    public class User
+    {
+        public static string conString = "Server=tcp:cldvst10390916sql-server.database.windows.net,1433;Initial Catalog=cldvst10390916sql-DB;Persist Security Info=False;User ID=Weylin;Password=6!n5J$J7asn7;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        public static SqlConnection con = new SqlConnection(conString);
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public int insert_User(User user)
+        {
+            string sql = "INSERT INTO Usertbl (user_name, user_surname, user_email, user_password) VALUES (@Name, @Surname, @Email, @Password)";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.Parameters.AddWithValue("@Name", user.Name);
+            cmd.Parameters.AddWithValue("@Surname", user.Surname);
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+            cmd.Parameters.AddWithValue("@Password", user.Password);
+            con.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            con.Close();
+            return rowsAffected;
+        }
+
+        public int SelectUser(string email, string password)
+        {
+            int userId = -1; // Default value if user is not found
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                string sql = "SELECT user_id FROM Usertbl WHERE user_email = @Email AND user_password = @Password";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Password", password);
+                try
+                {
+                    con.Open();
+                    object result = cmd.ExecuteScalar();
+                    if (result != null && result != DBNull.Value)
+                    {
+                        userId = Convert.ToInt32(result);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return userId;
+        }
+
+    }
+}
